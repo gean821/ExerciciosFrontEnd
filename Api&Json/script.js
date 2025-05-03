a185b29bbeaa1dcd144d4720030c0bceeb801aec
Gean Luca <gean.luca@ingacode.com.br>
2025-05-02 21:52:39 -0300

    -feat(JSON): Adiciona exercicio Json e LocalStorage

--- Api&Json/script.js
+++ Api&Json/script.js
@@ -1,22 +1,44 @@
 //Permitir que o usuário cadastre nome e email, exiba os dados em tela no formato JSON e mostre uma mensagem de validação se algum campo estiver vazio.
 const button = document.querySelector('#registerButton');
+const cleanButton = document.querySelector('#cleanButton');
+
 
 function buttonAction() {
+    Event.preventDefault();
+
     const login = {
         user : document.querySelector('#name').value.trim(),
         email : document.querySelector('#email').value.trim()
     };
+
     if (login.user === '' || login.email === '') {
         alert("Please fill in all fields to login");
+        return;
     }
+    
+    localStorage.setItem("login",JSON.stringify(login));
     const json = convertToJson(login);
     console.log(json);
+    
     printJson(json);
+    document.querySelector('#name').value = '';   //limpando o input após preenchimento para ficar clean
+    document.querySelector('#email').value = '';
+
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const savedLogin = localStorage.getItem("login");
+    
+    if (savedLogin) {
+        const print = document.querySelector('#usersList');
+        const li = document.createElement('li');
+        li.textContent = savedLogin; 
+        print.appendChild(li);
+    }
+});
 
 function convertToJson(login) {
     return JSON.stringify(login);
-    
 }
 
 function printJson(json) {
@@ -26,8 +48,15 @@ function printJson(json) {
     print.appendChild(h4);
 }
 
+function clearData() {
+    localStorage.clear();
+    const userList = document.querySelector('#usersList');
+    userList.textContent = '';
+}
+
 
 button.addEventListener('click', buttonAction);
+cleanButton.addEventListener('click', clearData);
 
 
 
---
+++ Json/SignInJson/script.js
@@ -0,0 +1,34 @@
+//Permitir que o usuário cadastre nome e email, exiba os dados em tela no formato JSON e mostre uma mensagem de validação se algum campo estiver vazio.
+const button = document.querySelector('#registerButton');
+
+function buttonAction() {
+    const login = {
+        user : document.querySelector('#name').value.trim(),
+        email : document.querySelector('#email').value.trim()
+    };
+    if (login.user === '' || login.email === '') {
+        alert("Please fill in all fields to login");
+    }
+    const json = convertToJson(login);
+    console.log(json);
+    printJson(json);
+    document.querySelector('#name').value = '';   //limpando o input após preenchimento para ficar clean
+    document.querySelector('#email').value = ''; 
+}
+
+function convertToJson(login) {
+    return JSON.stringify(login);
+}
+
+function printJson(json) {
+    const print = document.querySelector('.loginRegisters');
+    const h4 = document.createElement('h4');
+    h4.textContent = json;
+    print.appendChild(h4);
+}
+
+
+button.addEventListener('click', buttonAction);
+
+
+
